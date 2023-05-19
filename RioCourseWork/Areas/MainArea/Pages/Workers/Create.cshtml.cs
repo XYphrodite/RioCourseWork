d0dab609aec5da61ddebe2688a54401bd2f4f262
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-05-19 18:07:37 +0300

    9

--- RioCourseWork/Areas/MainArea/Pages/Workers/Create.cshtml.cs
+++ RioCourseWork/Areas/MainArea/Pages/Workers/Create.cshtml.cs
@@ -8,19 +8,17 @@ namespace RioCourseWork.Areas.MainArea.Workers
     public class CreateModel : PageModel
     {
         private readonly Repository repo;
-
         public CreateModel(Repository repo)
         {
             this.repo = repo;
         }
-
         public Person person { get; set; } = new Person();
 
         public IActionResult OnGetAsync()
         {
             return Page();
         }
-        public IActionResult OnPostAdd(string Name)
+        public IActionResult OnPostAdd()
         {
             return RedirectToAction("Index", "Home");
             //await repo.CreatePerson(person);
